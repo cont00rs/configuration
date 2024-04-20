691ffa1ed04e5691413e070cb8bb2bd601752b2c
cont00rs <cont00rs@users.noreply.github.com>
2024-04-20 08:51:56 +0200

    Small configuration tweaks

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -1,4 +1,5 @@
 require('orgmode').setup({
     org_agenda_files = {'~/vimwiki/**/*.org'},
     org_default_notes_file = '~/vimwiki/refile.org',
+    org_todo_keywords = {'TODO(t)', 'WAIT', '|', 'DONE', 'CANC'}
 })
