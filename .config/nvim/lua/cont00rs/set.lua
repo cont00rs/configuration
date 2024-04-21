c7bd1bde7c6fd054a42b5775ae329dca7932aae5
cont00rs <cont00rs@users.noreply.github.com>
2024-04-21 16:36:31 +0200

    Highlight trailing whitespace by listchars
    Enable list and set trailing whitespace to show up as small circle.

--- .config/nvim/lua/cont00rs/set.lua
+++ .config/nvim/lua/cont00rs/set.lua
@@ -24,35 +24,26 @@ vim.opt.path:append("**")
 
 vim.opt.wildmode = "list:longest"
 vim.opt.wildignore = "*o,*.swp,*.mod"
-vim.fn.matchadd("QuickFixLine", "/\\s/+$/")
-
-local mygroup = vim.api.nvim_create_augroup("MyGroup", { clear = false })
 
 -- Make trailing whitespaces explicit
-vim.api.nvim_set_hl(0, 'ExtraWhiteSpace', { bg = "darkred" })
-
-vim.api.nvim_create_autocmd({"BufWinEnter", "InsertLeave"}, {
-    group = "MyGroup",
-    pattern = "*",
-    command = [[match ExtraWhiteSpace /\s\+$/]]
+vim.opt.list = true
+vim.opt.listchars:append({
+    trail = "·",
 })
 
-vim.api.nvim_create_autocmd({"InsertEnter"}, {
-    group = "MyGroup",
-    pattern = "*",
-    command = [[match ExtraWhiteSpace /\s\+\%#\@<!$/]]
-})
+local _ = vim.api.nvim_create_augroup("MyGroup", { clear = false })
 
+-- Jump to old location on file reopen
 vim.api.nvim_create_autocmd(
-	"BufReadPost",
-	{callback = function()
-			local row, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
-			if {row, col} ~= {0, 0} then
-				vim.api.nvim_win_set_cursor(0, {row, 0})
-			end
-		end,
-	group = "MyGroup"
-	}
+    "BufReadPost",
+    {callback = function()
+            local row, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
+            if {row, col} ~= {0, 0} then
+                vim.api.nvim_win_set_cursor(0, {row, 0})
+            end
+        end,
+    group = "MyGroup"
+    }
 )
 
 vim.api.nvim_create_autocmd('LspAttach', {
