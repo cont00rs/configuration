152cddd27a5b95105a8d56385bd233dfeb032344
cont00rs <cont00rs@users.noreply.github.com>
2024-05-03 20:41:12 +0200

    Fix snippets, needed for some completions

--- .config/nvim/lua/cont00rs/lazy/lsp.lua
+++ .config/nvim/lua/cont00rs/lazy/lsp.lua
@@ -10,7 +10,7 @@ return {
         {'hrsh7th/cmp-buffer'},
         {'hrsh7th/cmp-path'},
         {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
+        {'L3MON4D3/LuaSnip',},
     },
 
     config = function()
--- .config/nvim/lua/cont00rs/lazy/telescope.lua
+++ .config/nvim/lua/cont00rs/lazy/telescope.lua
@@ -37,6 +37,7 @@ return {
         -- keymaps
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
