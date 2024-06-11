696213adad0b85455eda75c45f3bb29e2217e704
cont00rs <cont00rs@users.noreply.github.com>
2024-06-11 07:59:14 +0200

    feat: Also complete paths

--- .config/nvim/lua/cont00rs/lazy/lsp.lua
+++ .config/nvim/lua/cont00rs/lazy/lsp.lua
@@ -71,8 +71,8 @@ return {
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' },
-            }, {
-                { name = 'buffer' }
+                { name = 'path' },
+                { name = 'buffer' },
             })
         })
 
